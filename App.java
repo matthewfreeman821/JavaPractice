8e45d1d3a0225b0fb4c525435054ba0159c453c2
Matthew Freeman <matthew.freeman821@gmail.com>
2021-03-30 08:33:49 -0400

    Adds static add method and calls in main method in print line

--- App.java
+++ App.java
@@ -1,6 +1,12 @@
-public class App{
+public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println(add(3, 4));
+  }
+
+  public static int add(int a, int b) {
+    int sum = a + b;
+
+    return sum;
   }
 
 }
