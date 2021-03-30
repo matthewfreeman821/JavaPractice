bef458e1e3981f4557865fdfaff7f84d4f5bbf76
Matthew Freeman <matthew.freeman821@gmail.com>
2021-03-30 08:35:34 -0400

    Fixes subtract method to use subtract and calls method in main method

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 public class App {
   public static void main(String[] args) {
     System.out.println(add(3, 4));
+    System.out.println(subtract(3, 4));
   }
 
   public static int add(int a, int b) {
@@ -10,7 +11,7 @@ public class App {
   }
 
   public static int subtract(int a, int b) {
-    int diff = a + b;
+    int diff = a - b;
 
     return diff;
   }
