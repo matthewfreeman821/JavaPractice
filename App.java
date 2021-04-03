27694752c7a3e780d4da10eccec28ca446616c04
Matthew Freeman <matthew.freeman821@gmail.com>
2021-04-03 12:32:25 -0400

    Changes class to static and calls in main app method

--- App.java
+++ App.java
@@ -2,31 +2,13 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    names.add("Matthew");
-    names.add("Krista");
-    names.add("Maddie");
-    names.add("Ali");
-    // for (int i = 0; i < names.size(); i++) {
-    // System.out.println(names.get(i));
-    // }
-
-    // for (String i : names) {
-    // System.out.println(i);
-
-    // names.forEach(name -> System.out.println(name + " forEach loop"));
-
-    for (String i : names) {
-      if (i.equals("Maddie")) {
-        System.out.println("Name is " + i);
-      } else {
-        System.out.println("Name is not Maddie");
-      }
-    }
+    Person matthew = new Person("Matthew");
+    System.out.println(matthew.name);
   }
 
   static ArrayList<String> names = new ArrayList<>();
 
-  public class Person {
+  public static class Person {
     String name;
 
     public Person(String name) {
