c5420ddce839ede945654b7b4a12a9be1889512a
Matthew Freeman <matthew.freeman821@gmail.com>
2021-04-05 07:39:36 -0400

    Adds an age variable to class person as well as assigns it in the constructor

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ import javax.print.attribute.standard.PrinterName;
 
 public class App {
   public static void main(String[] args) {
-    Person matthew = new Person("Matthew");
+    Person matthew = new Person("Matthew", 34);
     Person.printName(matthew.name);
   }
 
@@ -12,9 +12,11 @@ public class App {
 
   public static class Person {
     String name;
+    int age;
 
-    public Person(String name) {
+    public Person(String name, int age) {
       this.name = name;
+      this.age = age;
     }
 
     public static void printName(String str) {
