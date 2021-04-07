6b494283180decff045a53df10d4e5585404c42d
Matthew Freeman <matthew.freeman821@gmail.com>
2021-04-07 07:29:59 -0400

    Adds a for loop which loops over a string and prints the associated number of the letter

--- App.java
+++ App.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
     Person matthew = new Person("Matthew", 34);
     matthew.printName(matthew.name);
     matthew.printAge(matthew.age);
+    for (int i = 0; i < 7; i++) {
+      System.out.println(matthew.name.codePointAt(i));
+    }
+    ;
   }
-
 }
