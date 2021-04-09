9cd5cc396e4bbbcd121e0f6f569c4ef6c1626cde
Matthew Freeman <matthew.freeman821@gmail.com>
2021-04-09 08:17:57 -0400

    Uses for loop to add items to person's bag

--- App.java
+++ App.java
@@ -4,13 +4,18 @@ import java.util.List;
 public class App {
   public static void main(String[] args) {
     List<String> bag = new ArrayList<>();
-    bag.add("Hello");
-    bag.add("World");
+    List<String> items = new ArrayList<>();
+
+    items.add("water");
+    items.add("food");
+    items.add("knife");
+    items.add("matches");
+    items.add("shovel");
+
     Person matthew = new Person("Matthew", 34, bag);
-    matthew.printName(matthew.name);
-    matthew.printAge(matthew.age);
-    for (int i = 0; i < bag.size(); i++) {
-      System.out.println(matthew.bag.get(i));
+
+    for (int i = 0; i < items.size(); i++) {
+      System.out.println(matthew.bag.add(items.get(i)));
     }
     ;
   }
