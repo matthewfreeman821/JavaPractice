76ba893a9e8d6e1ab5075a94ac7fc330a58fcff0
Matthew Freeman <matthew.freeman821@gmail.com>
2021-04-11 08:53:44 -0400

    Adds new variable, variable constructor, and public method to print the variable to person class

--- Person.java
+++ Person.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import jdk.tools.jlink.internal.plugins.StripNativeCommandsPlugin;
+
 public class Person {
   String name;
   int age;
   List<String> bag = new ArrayList();
+  String dob;
 
-  public Person(String name, int age, List bag) {
+  public Person(String name, int age, List bag, String dob) {
     this.name = name;
     this.age = age;
     this.bag = bag;
+    this.dob = dob;
   }
 
   public void printName(String str) {
@@ -18,5 +22,9 @@ public class Person {
 
   public void printAge(int age) {
     System.out.println("This person's age is " + age + ".");
+  }
+
+  public void printDob(String dob) {
+    System.out.println("This person's date of birth is " + dob + ".");
   }
 }
