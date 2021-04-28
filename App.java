8171b4fba38490bbdac94e79c759b7c100adf897
Matthew Freeman <matthew.freeman821@gmail.com>
2021-04-28 06:07:29 -0400

    Creates method for printing full name

--- App.java
+++ App.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+
+  public static void printFullName(String fName, String lName) {
+    System.out.println(fName + " " + lName);
+  }
+
   public static void main(String[] args) {
     List<String> bag = new ArrayList<>();
     Person matthew = new Person("Matthew", "Freeman", 34, bag, "7 June 1990");
@@ -13,16 +18,19 @@ public class App {
     matthew.addItemsToBag("knife", matthew.bag);
     matthew.addItemsToBag("string", matthew.bag);
 
-
-    if (matthew.bag.size() > 1) {
-      System.out.println(matthew.fName + "'s bag has " + matthew.bag.size() + " items in it.");
-      System.out.println("The items are: ");
-      matthew.bag.forEach(str -> System.out.println(matthew.fName + "'s bag has " + str + " in it."));
-    } else if (matthew.bag.size() == 1) {
-      System.out.println(matthew.fName + "'s bag has " + matthew.bag.size() + " item in it.");
-      matthew.bag.forEach(str -> System.out.println(matthew.fName + "'s bag has " + str + " in it."));
-    } else {
-      System.out.println(matthew.fName + "'s bag has no items in it.'");
-    }
+    // if (matthew.bag.size() > 1) {
+    // System.out.println(matthew.fName + "'s bag has " + matthew.bag.size() + "
+    // items in it.");
+    // System.out.println("The items are: ");
+    // matthew.bag.forEach(str -> System.out.println(matthew.fName + "'s bag has " +
+    // str + " in it."));
+    // } else if (matthew.bag.size() == 1) {
+    // System.out.println(matthew.fName + "'s bag has " + matthew.bag.size() + "
+    // item in it.");
+    // matthew.bag.forEach(str -> System.out.println(matthew.fName + "'s bag has " +
+    // str + " in it."));
+    // } else {
+    // System.out.println(matthew.fName + "'s bag has no items in it.'");
+    // }
   }
 }
