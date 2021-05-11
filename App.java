fd4882cf2aa4fd7213ce8af1f9f907e1430743d2
Matthew Freeman <matthew.freeman821@gmail.com>
2021-05-11 08:51:07 -0400

    Comments out old code

--- App.java
+++ App.java
@@ -8,21 +8,21 @@ public class App {
     System.out.println(matthew.fName);
     System.out.println(matthew.lName);
     matthew.printFullName(matthew.fName, matthew.lName);
-    matthew.printAge(age);
-    matthew.updateAge(33);
-    matthew.printAge(age);
-    matthew.printDob(dob);
+    // matthew.printAge(age);
+    // matthew.updateAge(33);
+    // matthew.printAge(age);
+    // matthew.printDob(dob);
 
-    matthew.addItemsToBag("water", matthew.bag);
-    matthew.addItemsToBag("shovel", matthew.bag);
-    matthew.addItemsToBag("knife", matthew.bag);
-    matthew.addItemsToBag("string", matthew.bag);
-    matthew.addItemsToBag("bottle", matthew.bag);
-    matthew.printBagItems(bag);
-    System.out.println("You have " + matthew.bag.size() + " items in your bag.");
-    matthew.removeLastItem(bag);
-    matthew.printBagItems(bag);
-    System.out.println("You have " + matthew.bag.size() + " items in your bag.");
+    // matthew.addItemsToBag("water", matthew.bag);
+    // matthew.addItemsToBag("shovel", matthew.bag);
+    // matthew.addItemsToBag("knife", matthew.bag);
+    // matthew.addItemsToBag("string", matthew.bag);
+    // matthew.addItemsToBag("bottle", matthew.bag);
+    // matthew.printBagItems(bag);
+    // System.out.println("You have " + matthew.bag.size() + " items in your bag.");
+    // matthew.removeLastItem(bag);
+    // matthew.printBagItems(bag);
+    // System.out.println("You have " + matthew.bag.size() + " items in your bag.");
 
   }
 }
