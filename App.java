1eb4356099f89cfb3a96e959de201db94dba9b23
Matthew Freeman <matthew.freeman821@gmail.com>
2021-05-14 06:50:56 -0400

    Adds new bag item and changes name of old bag item to fix bug

--- App.java
+++ App.java
@@ -3,23 +3,24 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    List<String> bag = new ArrayList<>();
-    Person matthew = new Person("Matthew", "Freeman", 34, bag, "7 June 1990");
-    Person maddie = new Person("Maddie", "Freeman", 8, bag, "20 March 2013");
+    List<String> matthewBag = new ArrayList<>();
+    List<String> maddieBag = new ArrayList<>();
+    Person matthew = new Person("Matthew", "Freeman", 34, matthewBag, "7 June 1990");
+    Person maddie = new Person("Maddie", "Freeman", 8, maddieBag, "20 March 2013");
     System.out.println(maddie.fName);
     System.out.println(matthew.fName);
     System.out.println(matthew.lName);
     matthew.printFullName(matthew.fName, matthew.lName);
 
     // Add item to maddie's bag
-    maddie.addItemsToBag("water", bag);
+    maddie.addItemsToBag("water", maddieBag);
 
     // Add item to matthew's bag
-    matthew.addItemsToBag("tent", bag);
+    matthew.addItemsToBag("tent", matthewBag);
 
     // Print bags to see if items saved correctly
     // Not saved correctly, need to update bag call
-    maddie.printBagItems(bag);
-    matthew.printBagItems(bag);
+    maddie.printBagItems(maddieBag);
+    matthew.printBagItems(matthewBag);
   }
 }
