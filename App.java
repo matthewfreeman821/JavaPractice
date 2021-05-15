21200b654bb3d05600575bb77c51bb5c0f2fefcf
Matthew Freeman <matthew.freeman821@gmail.com>
2021-05-15 13:26:21 -0400

    Adds new person and bag

--- App.java
+++ App.java
@@ -5,8 +5,10 @@ public class App {
   public static void main(String[] args) {
     List<String> matthewBag = new ArrayList<>();
     List<String> maddieBag = new ArrayList<>();
+    List<String> aliBag = new ArrayList<>();
     Person matthew = new Person("Matthew", "Freeman", 34, matthewBag, "7 June 1990");
     Person maddie = new Person("Maddie", "Freeman", 8, maddieBag, "20 March 2013");
+    Person ali = new Person("Ali", "Freeman", 4, aliBag, "4 Dec 2016");
     System.out.println(maddie.fName);
     System.out.println(matthew.fName);
     System.out.println(matthew.lName);
