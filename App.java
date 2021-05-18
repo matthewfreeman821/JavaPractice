7719069f83234405fb76440599ff96608cc5eee4
Matthew Freeman <matthew.freeman821@gmail.com>
2021-05-18 09:25:53 -0400

    Adds print age for ali

--- App.java
+++ App.java
@@ -12,6 +12,7 @@ public class App {
     maddie.printFullName(maddie.fName, maddie.lName);
     maddie.printAge(maddie.age);
     ali.printFullName(ali.fName, ali.lName);
+    ali.printAge(ali.age);
     matthew.printFullName(matthew.fName, matthew.lName);
     matthew.printAge(matthew.age);
 
