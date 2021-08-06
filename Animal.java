e919b7b13b849222b50dfe8a4085cb8d154d585e
Matthew Freeman <matthew.freeman821@gmail.com>
2021-08-06 07:45:28 -0400

    Adds variable and constructor for Animal class for practice

--- Animal.java
+++ Animal.java
@@ -1,8 +1,13 @@
 //Perhaps change to an abstract class?
 public class Animal {
   // Set variables to private
+  String name;
+
+  // Constructor???
+  public void Animal(String name) {
+    this.name = name;
+  }
 
   // Give public getters and setters
 
-  // Constructor???
 }
