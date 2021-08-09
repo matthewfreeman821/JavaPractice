07f85005c7c9e5e9615271752b141b87bc875460
Matthew Freeman <matthew.freeman821@gmail.com>
2021-08-09 05:26:04 -0400

    Removes comments and makes name in animal private

--- Animal.java
+++ Animal.java
@@ -1,14 +1,12 @@
 //Perhaps change to an abstract class?
 public class Animal {
   // Set variables to private
-  String name;
+  private String name;
 
-  // Constructor???
   public void Animal(String name) {
     this.name = name;
   }
 
-  // Give public getters and setters
   public string getName() {
     return this.name;
   }
