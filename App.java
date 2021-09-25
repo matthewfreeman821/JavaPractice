fb9363ac63cfb4809327dcfbed3eee75d87c2673
Matthew Freeman <matthew.freeman821@gmail.com>
2021-09-25 07:44:07 -0400

    Removes unecessary comment

--- App.java
+++ App.java
@@ -25,7 +25,6 @@ public class App {
     ali.updateFName("Alisa");
     ali.printFullName();
     ali.printNickname();
-    // System.out.println(ali.getNickname());
     ali.printNickname();
     ali.printAge();
     ali.printDob();
