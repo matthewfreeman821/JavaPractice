c6effe6ce65f7228f5db8367eee6c3c296fa3e56
Matthew Freeman <matthew.freeman821@gmail.com>
2021-10-01 07:08:40 -0400

    Formatting changes

--- Animal.java
+++ Animal.java
@@ -3,20 +3,20 @@ public class Animal {
   // Set variables to private
   private String name;
 
-  public void speak() {
-    System.out.println("Animal makes a sound");
-  }
-
-  private void walk() {
-    System.out.println("Animal walks");
-  }
-
   public void Animal() {
 
   }
 
   public void Animal(String name) {
     this.name = name;
+  }
+
+  public void speak() {
+    System.out.println("Animal makes a sound");
+  }
+
+  private void walk() {
+    System.out.println("Animal walks");
   }
 
   public String getName() {
