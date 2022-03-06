96973c9c13585589e018192a63f2acae61abd06a
Matthew Freeman <matthew.freeman821@gmail.com>
2022-03-06 08:05:19 -0500

    Adds comment

--- Person.java
+++ Person.java
@@ -153,6 +153,7 @@ public class Person extends Animal {
   // Setters and getters for snake
   public void setSnake(Snake snake) {
     if (null != snake.getOwner()) {
+      // Cannot set with null. Needs something else
       snake.getOwner().setSnake(null);
       snake.setOwner(this);
       this.snake = snake;
