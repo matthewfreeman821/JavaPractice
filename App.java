0e5d0b3ef9d12359a3fc9836a3d8d89df484c76f
Matthew Freeman <matthew.freeman821@gmail.com>
2022-04-08 10:50:49 -0400

    Updates to main App for future development

--- App.java
+++ App.java
@@ -3,17 +3,6 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    List<String> matthewBag = new ArrayList<>();
-    List<String> maddieBag = new ArrayList<>();
-    List<String> aliBag = new ArrayList<>();
-    Person matthew = new Person("Matthew", "Freeman", 34, matthewBag, "7 June 1990", "Matt");
-    Person maddie = new Person("Maddie", "Freeman", 8, maddieBag, "20 March 2013", "Maddie");
-    Person ali = new Person("Ali", "Freeman", 4, aliBag, "4 Dec 2016", "Ali");
-    Snake bowey = new Snake(ali);
-    Snake princess = new Snake(maddie);
-    Animal duke = new Animal(duke);
-
-    duke.speak();
-    bowey.speak();
+    System.out.println('Will be testing regex soon');
   }
 }
