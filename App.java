2b02d60b1044fd11fa848f05debf7eec419c2757
Matthew Freeman <matthew.freeman821@gmail.com>
2022-04-20 11:10:17 -0400

    removes stingUtils

--- App.java
+++ App.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
-import com.sun.tools.javac.util.StringUtils;
 
 public class App {
   public static void main(String[] args) {
@@ -11,10 +10,10 @@ public class App {
     final String REGEX_WHITELIST_1 = "^.*(\\r\\n|\\r|\\n).*$";
     final String TEST = "93\r\n2a";
 
-    System.out.println(StringUtiles.toString(regexValidation(TEST, REGEX_WHITELIST_1)));
+    regexValidation(TEST, REGEX_WHITELIST_1);
   }
 
   private static void regexValidation(String input, String regex) {
-    boolean isValid = StringUtils.isBlank(input) || !java.util.regex.Pattern.compile(regex).matcher(input).matches();
+    boolean isValid = java.util.regex.Pattern.compile(regex).matcher(input).matches();
   }
 }
