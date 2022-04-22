a1853b92d07f3510104bfe0fe75cb3301d9550ac
Matthew Freeman <matthew.freeman821@gmail.com>
2022-04-22 14:08:56 -0400

    Removes uneeded import

--- App.java
+++ App.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
-
 public class App {
   public static void main(String[] args) {
     System.out.println("Will be testing regex soon");
