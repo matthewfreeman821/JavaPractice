e3d19ba2a72c4f2e6cd9b970d090adffb662d7a9
Matthew Freeman <matthew.freeman821@gmail.com>
2022-05-02 09:58:41 -0400

    Adds new test string

--- App.java
+++ App.java
@@ -9,6 +9,7 @@ public class App {
     final String REGEX_2 = "^\\d$";
     final String TEST = "93\r\n2a";
     final String TEST_2 = "932";
+    final String TEST_3 = "abcDEF";
     ArrayList<String> stringsToTest = new ArrayList<String>();
     ArrayList<String> regexTests = new ArrayList<String>();
     regexTests.add(REGEX_1);
