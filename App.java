3bf3286d65e3910e456d3bb750a0e0a5cb0ab25a
Matthew Freeman <matthew.freeman821@gmail.com>
2022-05-10 10:07:17 -0400

    Removes println from beginning

--- App.java
+++ App.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Will be testing regex soon");
-
     final String REGEX_1 = "^.*(\\r\\n|\\r|\\n).*$";
     final String REGEX_2 = "^\\d$";
     final String TEST = "93\r\n2a";
