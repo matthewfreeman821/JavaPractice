56526f6e62d6049967e7c1769d87fdff25d52e70
Matthew Freeman <matthew.freeman821@gmail.com>
2022-07-07 10:50:01 -0400

    Moves var to constants file

--- Constants.java
+++ Constants.java
@@ -3,4 +3,5 @@ public class Constants {
   public final String REGEX_1 = "^.*(\\r\\n|\\r|\\n).*$";
   public final String REGEX_2 = "^\\/d*$";
   public final String REGEX_3 = "^[a-zA-Z]*$";
+  final String REGEX_4 = "^[@#$%^]*$";
 }
