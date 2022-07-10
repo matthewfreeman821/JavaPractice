160ceb8f90dcfc57ffa2c160a66f11c749ebfc41
Matthew Freeman <matthew.freeman821@gmail.com>
2022-07-10 10:02:34 -0400

    Changes modifier

--- Constants.java
+++ Constants.java
@@ -4,5 +4,5 @@ public class Constants {
   public final String REGEX_2 = "^\\/d*$";
   public final String REGEX_3 = "^[a-zA-Z]*$";
   public final String REGEX_4 = "^[@#$%^]*$";
-  final String REGEX_5 = "^[0-9]*$";
+  public final String REGEX_5 = "^[0-9]*$";
 }
