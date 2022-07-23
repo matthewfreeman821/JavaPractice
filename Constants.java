5439a66e1c3a221dd3d440bfdd2cc41145b8e340
Matthew Freeman <matthew.freeman821@gmail.com>
2022-07-23 10:45:06 -0400

    Updates field

--- Constants.java
+++ Constants.java
@@ -1,7 +1,7 @@
 // Will add constants to this file
 public class Constants {
   public static final String REGEX_1 = "^.*(\\r\\n|\\r|\\n).*$";
-  public final String REGEX_2 = "^\\/d*$";
+  public static final String REGEX_2 = "^\\/d*$";
   public final String REGEX_3 = "^[a-zA-Z]*$";
   public final String REGEX_4 = "^[@#$%^]*$";
   public final String REGEX_5 = "^[0-9]*$";
