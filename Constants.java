ef7b7534627a90b70ed22c6ebe97f2dc3199b732
Matthew Freeman <matthew.freeman821@gmail.com>
2022-07-25 08:38:46 -0400

    Updates field

--- Constants.java
+++ Constants.java
@@ -3,7 +3,7 @@ public class Constants {
   public static final String REGEX_1 = "^.*(\\r\\n|\\r|\\n).*$";
   public static final String REGEX_2 = "^\\/d*$";
   public static final String REGEX_3 = "^[a-zA-Z]*$";
-  public final String REGEX_4 = "^[@#$%^]*$";
+  public static final String REGEX_4 = "^[@#$%^]*$";
   public final String REGEX_5 = "^[0-9]*$";
 
   public final String TEST_1 = "93\r\n2a";
