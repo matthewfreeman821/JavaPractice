adabef46d01f0653f120078850e0d5d2c48bc0a2
Matthew Freeman <matthew.freeman821@gmail.com>
2022-07-27 11:26:31 -0400

    Updates field

--- Constants.java
+++ Constants.java
@@ -6,7 +6,7 @@ public class Constants {
   public static final String REGEX_4 = "^[@#$%^]*$";
   public static final String REGEX_5 = "^[0-9]*$";
 
-  public final String TEST_1 = "93\r\n2a";
+  public static final String TEST_1 = "93\r\n2a";
   public final String TEST_2 = "932";
   public final String TEST_3 = "abcDEF";
   public final String TEST_4 = "@#$%^";
