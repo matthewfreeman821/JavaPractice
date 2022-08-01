be2041d587b4ad35be49df1402890f1c02bcc3b8
Matthew Freeman <matthew.freeman821@gmail.com>
2022-08-01 17:03:11 -0400

    Fixes reference

--- App.java
+++ App.java
@@ -8,7 +8,7 @@ public class App {
     ArrayList<String> stringsToTest = new ArrayList<String>();
     ArrayList<String> regexTests = new ArrayList<String>();
     regexTests.add(Constants.REGEX_1);
-    regexTests.add(REGEX_2);
+    regexTests.add(Constants.REGEX_2);
     regexTests.add(REGEX_3);
     regexTests.add(REGEX_4);
     regexTests.add(REGEX_5);
